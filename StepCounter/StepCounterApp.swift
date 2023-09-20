dd6305623c2cc4444a0509b398e1383b1498b32b
Lele Luu <leleluu@gmail.com>
2023-09-20 12:19:24 +0200

    Initial Commit

---
+++ StepCounter/StepCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepCounterApp.swift
+//  StepCounter
+//
+//  Created by lele luu on 20.09.23.
+//
+
+import SwiftUI
+
+@main
+struct StepCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
